16d5baf527f8080f24b414a5f7ab8adb2d69be3b
Alexander Chapchuk <BigTows@yandex.ru>
2023-05-19 18:39:52 +0300

    Change server name,

---
+++ src/main/java/com/example/example/controller/AmqpController.java
@@ -0,0 +1,23 @@
+package com.example.example.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller
+public class AmqpController {
+
+
+//    @RabbitListener(queues = "notification")
+//    public void processConfirmEmailQuery(final Notification notification) {
+//
+//        log.info("Accepted notification");
+//        try {
+//            notificationService.submitNotification(notification);
+//        } catch (AbuseEmailException e) {
+//            log.error("Skill notification for {}", notification.getNotificationOwner());
+//        }
+//        logger.info("Notification submitted");
+//    }
+
+}
